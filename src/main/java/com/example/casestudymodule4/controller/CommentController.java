388161e897337562b60d73c2e3b813f2b6653178
Quocanh <phiquocanh717@gmail.com>
2021-08-06 17:18:10 +0700

    done apiCommnent

--- src/main/java/com/example/casestudymodule4/controller/CommentController.java
+++ src/main/java/com/example/casestudymodule4/controller/CommentController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/Comment")
+@CrossOrigin("*")
 public class CommentController {
     @Autowired
     private ICommentService commentService;
