b74da36ccac25ff9a3ea0b1039a3b862f2534210
dinhhuychuong3005 <dinhhuychuong3005@gmail.com>
2021-08-09 18:29:35 +0700

    ok aem

--- Forntend_Facebook/workwisehtml-10/HTML/checkToken.js
+++ Forntend_Facebook/workwisehtml-10/HTML/checkToken.js
@@ -1,21 +1,25 @@
-function setItem(){
+function setItem() {
     window.localStorage.removeItem("token")
     window.localStorage.removeItem("total")
 }
+
 function checkTonke() {
     console.log(localStorage.getItem("token"))
     if (localStorage.getItem("token") == null) {
         window.location.href = "sign-in.html"
     }
 }
-    function getUserById1() {
-        $.ajax({
-            type: "GET",
-            url: "http://localhost:8080/api/" + localStorage.getItem("id"),
-            headers: {"Authorization": 'Bearer ' + localStorage.getItem("token")},
-            success: function (data) {
-                localStorage.setItem("fullName", data.fullName)
 
-            }
-        })
-    }
+function getUserById1(id) {
+    console.log(id)
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/api/" + id,
+        headers: {"Authorization": 'Bearer ' + localStorage.getItem("token")},
+        success: function (data) {
+            console.log(data.fullName)
+            localStorage.setItem("fullNameUs", data.fullName)
+            document.getElementById("name").innerHTML = data.fullName;
+        }
+    })
+}
